6565a2ca19141d442a2cd606acc8c899104b07c1
emilio castano <219035709@mycput.ac.za>
2021-05-16 22:57:42 +0200

    Added Collection

---
+++ src/test/java/ac/za/cput/StudentCollectionTest.java
@@ -0,0 +1,20 @@
+package ac.za.cput;
+
+
+import java.util.Collection;
+import java.util.LinkedHashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Student Number: 219035709
+ * Author: Emilio Castano
+ * Date Due: 16/5/2021
+ */
+class StudentCollectionTest {
+
+    Collection sn = new LinkedHashSet();
+
+    public void setSn(Collection sn) {
+        this.sn = sn;
+    }
+}
